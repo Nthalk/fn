67761960433c75238940547c44a719d64ad7b885
Carl Taylor <carl@etaylor.me>
2016-03-13 21:04:19 -0700

    Removed callable helper for generator

--- src/main/java/com/iodesystems/fn/Generator.java
+++ src/main/java/com/iodesystems/fn/Generator.java
@@ -2,22 +2,4 @@ package com.iodesystems.fn;
 
 public interface Generator<T> {
     T next();
-
-    abstract class Callable<T> implements Generator {
-        private final java.util.concurrent.Callable<T> callable;
-
-        public Callable(java.util.concurrent.Callable<T> callable) {
-            this.callable = callable;
-        }
-
-        @Override
-        public T next() {
-            try {
-                return callable.call();
-            } catch (Exception e) {
-                // Uhoh...
-                return null;
-            }
-        }
-    }
 }
